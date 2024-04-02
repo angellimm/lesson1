649378a8ee31f5a79f0c4340ec7241c463e5af3b
angel <legosalo@yandex.ru>
2024-04-02 14:10:28 +0300

    added task3

--- task3/obj/Debug/net8.0/task3.AssemblyInfo.cs
+++ task3/obj/Debug/net8.0/task3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fba4a865637d30b0e5d4ce821ae2d974a31cdba5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c35f2fb487e418a18860abfd86b29fcfee7bd409")]
 [assembly: System.Reflection.AssemblyProductAttribute("task3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
